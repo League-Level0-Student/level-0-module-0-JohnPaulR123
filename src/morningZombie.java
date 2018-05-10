b63febe29f7f846f753a87ec2c4af3628bc700be
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-10 12:48:16 -0700

    Completed assignment morning zombie

---
+++ src/morningZombie.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class morningZombie {
+	public static void main(String[] args) {
+JOptionPane.showMessageDialog(null, " Get out of bed!");
+JOptionPane.showMessageDialog(null, " Why?");
+JOptionPane.showMessageDialog(null, "You're going to late for school");
+	}
+}
